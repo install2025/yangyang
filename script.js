174237aa47731a4d97a7118af1957e5accbc8371
install2025 <2210673625@qq.com>
2025-02-27 01:03:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const noTexts = [
     "？你认真的吗…", 
     "要不再想想？", 
     "不许选这个！ ", 
-    "我会很伤心…", 
+    "小阳很伤心…", 
     "不行:("
 ];
 
@@ -49,7 +49,7 @@ noButton.addEventListener("click", function() {
 yesButton.addEventListener("click", function() {
     document.body.innerHTML = `
         <div class="yes-screen">
-            <h1 class="yes-text">!!!喜欢你!! ( >᎑<)♡︎ᐝ</h1>
+            <h1 class="yes-text">!!!爱小九!! ( >᎑<)♡︎ᐝ</h1>
             <img src="images/hug.png" alt="拥抱" class="yes-image">
         </div>
     `;
